6fd184c3d44d360f08e2d42e1afac4c44e5fb279
Seean Kim <sekim@hubspot.com>
2026-05-15 11:16:28 -0400

    add deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://Skim-chee.github.io',
+  base: '/Apex2026',
 	// Enable Preact to support Preact JSX components.
 	integrations: [preact()],
 });
